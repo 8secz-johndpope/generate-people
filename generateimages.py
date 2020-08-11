aaf34c470fc3fddf76f8ad81cd30bbbb601c6f2d
Jonathan Watson <jonathanw12@outlook.com>
2020-08-11 13:05:41 +0100

    Add path selection

--- generateimages.py
+++ generateimages.py
@@ -1,12 +1,28 @@
-from random import randint
+from numpy.random import randint
 import sys, os
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "stylegan2"))
-from stylegan2.run_generator import generate_images
+path = os.path.join(os.path.dirname(__file__), "stylegan2")
+sys.path.append(path)
+
+import stylegan2.dnnlib as dnnlib
 
 
-network_pkl = "gdrive:networks/stylegan2-ffhq-config-f.pkl"
-seeds = [randint(1, 10000)]
-truncation_psi = 0.5
+def generate_images(seeds):
+    sc = dnnlib.SubmitConfig()
+    sc.num_gpus = 1
+    sc.submit_target = dnnlib.SubmitTarget.LOCAL
+    sc.local.do_not_copy_source_files = True
+    sc.run_dir_root = "dist/generated-images-1"
+    sc.run_desc = "generate_images"
 
-generate_images(network_pkl, seeds, truncation_psi)
+    kwargs = {
+        "network_pkl": "gdrive:networks/stylegan2-ffhq-config-f.pkl",
+        "seeds": seeds,
+        "truncation_psi": 0.5,
+    }
+
+    dnnlib.submit_run(sc, "run_generator.generate_images", **kwargs)
+
+
+if __name__ == "__main__":
+    generate_images(randint(100000000, 1000000000, 5))
